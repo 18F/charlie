00d796b43de09a18016baa23dee955625dcf622f
Bret Mogilefsky <bmogilefsky@gmail.com>
2016-10-20 13:54:17 -0700

    Whoops, wrong replacement

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export PORT=$PORT
-export BIND_ADDRESS=$HOST
+export BIND_ADDRESS=0.0.0.0
 export PATH=./node_modules/.bin:$PATH
 ./node_modules/.bin/hubot --adapter slack --name charlie
