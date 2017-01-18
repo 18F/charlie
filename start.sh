8a813ec1eacfc656037ba16b2c2dd4fad72fe233
Greg Walker <michael.walker@gsa.gov>
2017-01-18 09:19:04 -0600

    Explain that redis doesn't use usernames

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@
 redishost=$(echo $VCAP_SERVICES | ./node_modules/.bin/json "redis28[0].credentials.hostname")
 redisport=$(echo $VCAP_SERVICES | ./node_modules/.bin/json "redis28[0].credentials.port")
 redispass=$(echo $VCAP_SERVICES | ./node_modules/.bin/json "redis28[0].credentials.password")
-export REDIS_URL="redis://dummy:$redispass@$redishost:$redisport"
+
+# Redis doesn't use usernames, so it can be anything.
+export REDIS_URL="redis://anyvalue:$redispass@$redishost:$redisport"
 
 export PORT=$PORT
 export BIND_ADDRESS=0.0.0.0
