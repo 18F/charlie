671405d140b27a4dabc8542f0c326b27954c534d
Greg Walker <mgwalker@gmail.com>
2017-08-09 20:09:57 -0500

    Clean up a bit

--- scripts/gsa-approved-software.js
+++ scripts/gsa-approved-software.js
@@ -20,6 +20,14 @@ const colors = {
   'Denied': '#e31c3d'
 };
 
+const getCSV = () => {
+  return new Promise((resolve, reject) => {
+    _robot.http('https://raw.githubusercontent.com/GSA/data/gh-pages/enterprise-architecture/it-standards.csv')
+      .header('User-Agent', '18F-bot')
+      .get()((err, res, body) => resolve(body));
+  });
+};
+
 const getColor = (status) => {
   const shortStatus = status.match(/^\S+/)[0];
   if (colors[shortStatus]) {
@@ -41,29 +49,27 @@ const getAttachment = item => ({
 
 const handler = (msg) => {
   const lookingFor = msg.match[1].toLowerCase();
-  _robot.http('https://raw.githubusercontent.com/GSA/data/gh-pages/enterprise-architecture/it-standards.csv')
-    .header('User-Agent', '18F-bot')
-    .get()((err, res, body) => {
-      const matches = [];
-      csv().fromString(body)
-        .on('csv', (item) => {
-          if(item[0].toLowerCase().includes(lookingFor)) {
-            matches.push({ name: item[0], status: item[3], platform: item[4] })
-          }
-        }).on('done', () => {
-          let message = ''
-          if (matches.length == 0) {
-            message = `I didn't find anything in the GSA IT Standards for ${msg.match[1]}`;
-          } else if (matches.length <= 5) {
-            message = { attachments: matches.map(getAttachment) };
-            message.attachments[0].pretext = `Here's what I found in the GSA IT Standards`;
-          } else {
-            const finds = matches.map((item) => `*${item.name}* (${item.status})`)
-            message = `I found several potential matches for ${msg.match[1]}: ${finds.join(' | ')}`;
-          }
-          msg.send(message);
-        });
+  getCSV.then(body => {
+    const matches = [];
+    csv().fromString(body)
+      .on('csv', (item) => {
+        if(item[0].toLowerCase().includes(lookingFor)) {
+          matches.push({ name: item[0], status: item[3], platform: item[4] })
+        }
+      }).on('done', () => {
+        let message = ''
+        if (matches.length == 0) {
+          message = `I didn't find anything in the GSA IT Standards for ${msg.match[1]}`;
+        } else if (matches.length <= 5) {
+          message = { attachments: matches.map(getAttachment) };
+          message.attachments[0].pretext = `Here's what I found in the GSA IT Standards`;
+        } else {
+          const finds = matches.map((item) => `*${item.name}* (${item.status})`)
+          message = `I found several potential matches for ${msg.match[1]}: ${finds.join(' | ')}`;
+        }
+        msg.send(message);
       });
+    });
 };
 
 module.exports = (robot) => {
@@ -71,18 +77,3 @@ module.exports = (robot) => {
   robot.respond(/is (.+) approved/i, handler);
   robot.hear(/(\S+) is approved\?/i, handler)
 };
-
-    // # robot.http('https://api.github.com/repos/18f/procurement-glossary/contents/abbreviations.yml')
-    // #   .header('User-Agent', '18F-bot')
-    // #   .get() (err, res, body) ->
-    // #     b = new Buffer(JSON.parse(body).content, 'base64');
-    // #     g = yaml.safeLoad(b.toString(), { json: true }).abbreviations
-    // #
-    // #     searchTerm = msg.match[2].trim()
-    // #     terms = Object.keys(g)
-    // #     term = findCaseInsensitively(terms, searchTerm)
-    // #
-    // #     if term
-    // #       msg.reply "The term #{ g[term].longform } (#{ term }) means #{g[term].description}"
-    // #     else
-    // #       msg.reply "I don't know that term. If you'd like to add it, the project is at https://github.com/18F/procurement-glossary."
