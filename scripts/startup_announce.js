19f32c4105c00615944fe954adf10f48733159c0
Greg Walker <michael.walker@gsa.gov>
2018-02-13 09:41:43 -0600

    remove timeout

--- scripts/startup_announce.js
+++ scripts/startup_announce.js
@@ -4,10 +4,8 @@
 const target = process.env.HUBOT_STATUS_CHANNEL || 'bots';
 
 module.exports = (robot) => {
-  setTimeout(() => {
-    robot.messageRoom(target, {
-      target,
-      text: 'Johnny 5 is alliiiiiive!  Also I have just booted up.'
-    })
-  }, 2000);
+  robot.messageRoom(target, {
+    target,
+    text: 'Johnny 5 is alliiiiiive!  Also I have just booted up.'
+  })
 };
--- test/scripts/startup_announce.js
+++ test/scripts/startup_announce.js
@@ -11,28 +11,18 @@ describe('startup announcement', () => {
     delete require.cache[require.resolve('../../scripts/startup_announce')];
   });
 
-  it('announces itself within 2 seconds of starting up, using default channel', () => {
+  it('announces itself when starting up, using default channel', () => {
     const announceSetup = require('../../scripts/startup_announce');
     announceSetup(robot);
 
-    return new Promise(resolve => {
-      setTimeout(() => {
-        expect(robot.messageRoom.calledWith('bots', { target: 'bots', text: sinon.match.string })).to.be.true;
-        resolve();
-      }, 2100);
-    });
-  }).timeout(2500);
+    expect(robot.messageRoom.calledWith('bots', { target: 'bots', text: sinon.match.string })).to.be.true;
+  });
 
   it('uses the channel value from the environment, if provided', () => {
     process.env.HUBOT_STATUS_CHANNEL = 'status-channel'
     const announceSetup = require('../../scripts/startup_announce');
     announceSetup(robot);
 
-    return new Promise(resolve => {
-      setTimeout(() => {
-        expect(robot.messageRoom.calledWith('status-channel', { target: 'status-channel', text: sinon.match.string })).to.be.true;
-        resolve();
-      }, 2100);
-    });
-  }).timeout(2500);
+    expect(robot.messageRoom.calledWith('status-channel', { target: 'status-channel', text: sinon.match.string })).to.be.true;
+  });
 });
