2588c2698ea347b3bb019f1bb947724627786eda
Greg Walker <michael.walker@gsa.gov>
2019-04-18 15:17:07 -0500

    fix up mpim.open call; use ephemeral responses; tests

--- scripts/coffeemate.coffee
+++ scripts/coffeemate.coffee
@@ -1,43 +1,57 @@
-request = require('request')
+defaultWebClient = require("@slack/client").WebClient
 
-module.exports = (robot) ->
+module.exports = (robot, { WebClient = defaultWebClient } = {}) ->
+	webAPI = new WebClient robot.adapter.options.token
 	queue = robot.brain.get("coffeemate_queue") || []
+
 	robot.hear /coffee me/i, (res) ->
 		# First, is the current user in already in the queue?
 		# If so, just let them know
-		console.log("the current user is", res.message.user.id)
-		console.log("the current queue is", queue)
 		if res.message.user.id in queue
-			res.reply "You're already in the queue. " +
-				"As soon as we find someone else to meet with, we'll introduce you."
-			#return
+			webAPI.chat.postEphemeral
+				channel: res.message.room
+				user: res.message.user.id
+				text: "You’re already in the queue. As soon as we find someone else to meet with, we’ll introduce you!"
+				as_user: true
+			return
+
 		# If we didn't bail out already, add the current user to the queue
 		queue.push(res.message.user.id)
 		robot.brain.set "coffeemate_queue", queue
 		robot.brain.save()
+		
 		# Now do we have a pair or not?
 		if queue.length < 2
-			res.reply "You’re in line for coffee! " +
-				"You’ll be introduced to the next person who wants to meet up."
+			webAPI.chat.postEphemeral
+				channel: res.message.room
+				user: res.message.user.id
+				text: "You’re in line for coffee! You’ll be introduced to the next person who wants to meet up."
+				as_user: true
 		else
 			# pair them up
-			res.reply "You’ve been matched up for coffee with <@" + queue[0] + ">! " +
-				"I’ll start a direct message for you two. :coffee: :tada:"
-			# Now start a 1:1 DM chat between the people in queue.
-			options = {
-				token: process.env.HUBOT_SLACK_TOKEN,
-				users: queue.toString()
-			}
+			webAPI.chat.postEphemeral
+				channel: res.message.room
+				user: res.message.user.id
+				text: "You’ve been matched up for coffee with <@" + queue[0] + ">! "
+				as_user: true
 
-			mpim = robot.adapter.client.web.mpim.open { options }, (err, mpim) ->
+			# Now start a 1:1 DM chat between the people in queue.
+			robot.adapter.client.web.mpim.open queue.join(','), (err, mpim) ->
 				if err or !mpim.ok
 					console.log("Error with Slack API", err)
-				
-				msg = "You two have been paired up for coffee." +
-						"The next step is to figure out a time that works for both of you." +
+					return
+
+				msg = "You two have been paired up for coffee. " +
+						"The next step is to figure out a time that works for both of you. " +
 						"Enjoy! :coffee:"
 
-				mpim.send msg
+				# mpim.send msg
+				robot.messageRoom mpim.group.id,
+					text: msg
+					username: "coffeemate"
+					icon_emoji: ":coffee:"
+					as_user: false
+
 				
 				# then empty the queue again
 				robot.brain.set "coffeemate_queue", []
---
+++ test/scripts/coffeemate.js
@@ -0,0 +1,262 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const coffeemate = require('../../scripts/coffeemate');
+
+describe('coffeemate', () => {
+  const sandbox = sinon.createSandbox();
+
+  const robot = {
+    adapter: {
+      client: {
+        web: {
+          mpim: {
+            open: sandbox.stub()
+          }
+        }
+      },
+      options: {
+        token: 'SLACK TOKEN'
+      }
+    },
+
+    brain: {
+      get: sandbox.stub(),
+      save: sandbox.spy(),
+      set: sandbox.spy()
+    },
+
+    hear: sandbox.spy(),
+
+    messageRoom: sandbox.spy()
+  };
+
+  const webAPI = {
+    chat: {
+      postEphemeral: sandbox.spy()
+    }
+  };
+  const WebClient = sinon.stub().returns(webAPI);
+
+  beforeEach(() => {
+    sandbox.resetBehavior();
+    sandbox.resetHistory();
+  });
+
+  it('subscribes to "coffee me" messages and loads the existing queue from the brain', () => {
+    coffeemate(robot, { WebClient });
+    expect(robot.hear.calledWith(/coffee me/i, sinon.match.func)).to.equal(
+      true
+    );
+    expect(robot.brain.get.calledWith('coffeemate_queue')).to.equal(true);
+  });
+
+  describe('when the coffee queue is initially empty', () => {
+    let handler;
+    before(() => {
+      coffeemate(robot, { WebClient });
+      handler = robot.hear.args[0][1];
+    });
+
+    it('sends an ephemeral message the first time a user asks for coffee and adds them to the queue', () => {
+      handler({
+        message: {
+          user: {
+            id: 'user 1'
+          },
+          room: 'room id'
+        }
+      });
+
+      expect(
+        webAPI.chat.postEphemeral.calledWith({
+          channel: 'room id',
+          user: 'user 1',
+          text:
+            'You’re in line for coffee! You’ll be introduced to the next person who wants to meet up.',
+          as_user: true
+        })
+      ).to.equal(true);
+
+      expect(
+        robot.brain.set.calledWith('coffeemate_queue', ['user 1'])
+      ).to.equal(true);
+      expect(robot.brain.save.calledAfter(robot.brain.set)).to.equal(true);
+    });
+
+    it('sends an ephemeral message subsequent time a user asks for coffee and does nothing to the queue', () => {
+      handler({
+        message: {
+          user: {
+            id: 'user 1'
+          },
+          room: 'room id'
+        }
+      });
+
+      expect(
+        webAPI.chat.postEphemeral.calledWith({
+          channel: 'room id',
+          user: 'user 1',
+          text:
+            'You’re already in the queue. As soon as we find someone else to meet with, we’ll introduce you!',
+          as_user: true
+        })
+      ).to.equal(true);
+
+      expect(robot.brain.set.notCalled).to.equal(true);
+      expect(robot.brain.save.notCalled).to.equal(true);
+    });
+
+    it('sends an ephemeral message, opens a DM, and resets the queue when a different user asks for coffee', () => {
+      // Have the mpim.open method call back
+      robot.adapter.client.web.mpim.open.yields(null, {
+        ok: true,
+        group: {
+          id: 'dm id'
+        }
+      });
+
+      handler({
+        message: {
+          user: {
+            id: 'user 2'
+          },
+          room: 'room id'
+        }
+      });
+
+      expect(
+        webAPI.chat.postEphemeral.calledWith({
+          channel: 'room id',
+          user: 'user 2',
+          text: 'You’ve been matched up for coffee with <@user 1>! ',
+          as_user: true
+        })
+      ).to.equal(true);
+
+      // DM is opened...
+      expect(
+        robot.adapter.client.web.mpim.open.calledWith('user 1,user 2')
+      ).to.equal(true);
+
+      // ...and message sent
+      expect(
+        robot.messageRoom.calledWith('dm id', {
+          text:
+            'You two have been paired up for coffee. The next step is to figure out a time that works for both of you. Enjoy! :coffee:',
+          username: 'coffeemate',
+          icon_emoji: ':coffee:',
+          as_user: false
+        })
+      );
+
+      expect(robot.brain.set.calledWith('coffeemate_queue', [])).to.equal(true);
+      expect(robot.brain.save.calledAfter(robot.brain.set)).to.equal(true);
+    });
+  });
+
+  describe('when the coffee queue begins with a user in it', () => {
+    let handler;
+    before(() => {
+      robot.brain.get.withArgs('coffeemate_queue').returns(['user 1']);
+      coffeemate(robot, { WebClient });
+      handler = robot.hear.args[0][1];
+    });
+
+    it('sends an ephemeral message the first time a user asks for coffee and does nothing to the queue', () => {
+      handler({
+        message: {
+          user: {
+            id: 'user 1'
+          },
+          room: 'room id'
+        }
+      });
+
+      expect(
+        webAPI.chat.postEphemeral.calledWith({
+          channel: 'room id',
+          user: 'user 1',
+          text:
+            'You’re already in the queue. As soon as we find someone else to meet with, we’ll introduce you!',
+          as_user: true
+        })
+      ).to.equal(true);
+
+      expect(robot.brain.set.notCalled).to.equal(true);
+      expect(robot.brain.save.notCalled).to.equal(true);
+    });
+
+    it('sends an ephemeral message subsequent time a user asks for coffee and does nothing to the queue', () => {
+      handler({
+        message: {
+          user: {
+            id: 'user 1'
+          },
+          room: 'room id'
+        }
+      });
+
+      expect(
+        webAPI.chat.postEphemeral.calledWith({
+          channel: 'room id',
+          user: 'user 1',
+          text:
+            'You’re already in the queue. As soon as we find someone else to meet with, we’ll introduce you!',
+          as_user: true
+        })
+      ).to.equal(true);
+
+      expect(robot.brain.set.notCalled).to.equal(true);
+      expect(robot.brain.save.notCalled).to.equal(true);
+    });
+
+    it('sends an ephemeral message, opens a DM, and resets the queue when a different user asks for coffee', () => {
+      // Have the mpim.open method call back
+      robot.adapter.client.web.mpim.open.yields(null, {
+        ok: true,
+        group: {
+          id: 'dm id'
+        }
+      });
+
+      handler({
+        message: {
+          user: {
+            id: 'user 2'
+          },
+          room: 'room id'
+        }
+      });
+
+      expect(
+        webAPI.chat.postEphemeral.calledWith({
+          channel: 'room id',
+          user: 'user 2',
+          text: 'You’ve been matched up for coffee with <@user 1>! ',
+          as_user: true
+        })
+      ).to.equal(true);
+
+      // DM is opened...
+      expect(
+        robot.adapter.client.web.mpim.open.calledWith('user 1,user 2')
+      ).to.equal(true);
+
+      // ...and message sent
+      expect(
+        robot.messageRoom.calledWith('dm id', {
+          text:
+            'You two have been paired up for coffee. The next step is to figure out a time that works for both of you. Enjoy! :coffee:',
+          username: 'coffeemate',
+          icon_emoji: ':coffee:',
+          as_user: false
+        })
+      );
+
+      expect(robot.brain.set.calledWith('coffeemate_queue', [])).to.equal(true);
+      expect(robot.brain.save.calledAfter(robot.brain.set)).to.equal(true);
+    });
+  });
+});
