d2cddd474a7fafba0dde43c979cea7825fa25076
Greg Walker <michael.walker@gsa.gov>
2020-05-19 13:58:45 -0500

    comment

--- scripts/what-day-is-it.js
+++ scripts/what-day-is-it.js
@@ -1,3 +1,9 @@
+/* This is a joke bot, in reference to how time never seems to pass since March
+   2020. This is one of our coping mechanisms during the COVID-19 pandemic.
+   Future maintainers, please be gentle with us. We're doing the best we can
+   with what we've got.
+*/
+
 const moment = require('moment-timezone');
 
 const march1 = moment.tz('2020-03-01T00:00:00Z', 'America/New_York');
