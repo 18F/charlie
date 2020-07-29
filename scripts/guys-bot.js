5c874899ecfef816828725cd12409e17f67553d4
Greg Walker <michael.walker@gsa.gov>
2020-07-29 12:40:31 -0500

    add more collective nouns

--- scripts/guys-bot.js
+++ scripts/guys-bot.js
@@ -10,6 +10,8 @@ const phrasesToIgnore = [
   /guy(s|z)bot(s|z)?/gi,
 ];
 
+const collectiveNouns = ["fellow kids", "folks", "humans", "people", `y'all`];
+
 module.exports = (robot) => {
   const { addEmojiReaction, postEphemeralMessage } = utils.setup(robot);
 
@@ -25,11 +27,14 @@ module.exports = (robot) => {
 
     addEmojiReaction("inclusion-bot", msg.message.room, msg.message.id);
 
+    const noun =
+      collectiveNouns[Math.floor(Math.random() * collectiveNouns.length)];
+
     postEphemeralMessage({
       attachments: [
         {
           color: "#2eb886",
-          pretext: `Did you mean *y'all*? (_<https://web.archive.org/web/20170714141744/https://18f.gsa.gov/2016/01/12/hacking-inclusion-by-customizing-a-slack-bot/|What's this?>_)`,
+          pretext: `Did you mean *${noun}*? (_<https://web.archive.org/web/20170714141744/https://18f.gsa.gov/2016/01/12/hacking-inclusion-by-customizing-a-slack-bot/|What's this?>_)`,
           text: `Hello! Our inclusive TTS culture is built one interaction at a time, and inclusive language is the foundation. Instead of guys, we encourage everyone to try out a new phrase to describe multiple people. This is a small way we build inclusion into our everyday work lives.          `,
           fallback: `Hello! Our inclusive TTS culture is built one interaction at a time, and inclusive language is the foundation. Instead of guys, we encourage everyone to try out a new phrase to describe multiple people. This is a small way we build inclusion into our everyday work lives.`,
         },
--- test/scripts/guys-bot.js
+++ test/scripts/guys-bot.js
@@ -49,7 +49,7 @@ describe("Inclusion/guys bot", () => {
       attachments: [
         {
           color: "#2eb886",
-          pretext: `Did you mean *y'all*? (_<https://web.archive.org/web/20170714141744/https://18f.gsa.gov/2016/01/12/hacking-inclusion-by-customizing-a-slack-bot/|What's this?>_)`,
+          pretext: sinon.match.string, // `Did you mean *y'all*? (_<https://web.archive.org/web/20170714141744/https://18f.gsa.gov/2016/01/12/hacking-inclusion-by-customizing-a-slack-bot/|What's this?>_)`,
           text: `Hello! Our inclusive TTS culture is built one interaction at a time, and inclusive language is the foundation. Instead of guys, we encourage everyone to try out a new phrase to describe multiple people. This is a small way we build inclusion into our everyday work lives.          `,
           fallback: `Hello! Our inclusive TTS culture is built one interaction at a time, and inclusive language is the foundation. Instead of guys, we encourage everyone to try out a new phrase to describe multiple people. This is a small way we build inclusion into our everyday work lives.`,
         },
