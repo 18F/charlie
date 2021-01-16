22897a47b638d3005c9acb182b42875f9add56b6
Greg Walker <mgwalker@gmail.com>
2021-01-15 20:26:19 -0600

    be prepared for the end of march 2020

--- scripts/what-day-is-it.js
+++ scripts/what-day-is-it.js
@@ -4,20 +4,36 @@
    with what we've got.
 */
 
-const moment = require('moment-timezone');
+const moment = require("moment-timezone");
 
-const march1 = moment.tz('2020-03-01T00:00:00Z', 'America/New_York');
+const march1 = moment.tz("2020-03-01T00:00:00Z", "America/New_York");
+const endOfMarch = moment.tz("2021-01-19T23:59:59Z", "America/New_York");
 
-module.exports = robot => {
-  robot.respond(/what day is it/i, msg => {
-    const now = moment.tz('America/New_York');
-     
-    const isBlursday = Math.random() < 0.2;
-    const isEvermarch = Math.random() < 0.2;
+module.exports = (robot) => {
+  robot.respond(/what day is it/i, (msg) => {
+    const now = moment.tz("America/New_York");
 
-    const dow = isBlursday ? 'Blursday' : now.format('dddd');
-    const days = Math.ceil(moment.duration(now.diff(march1)).as('days'));
+    if (now.isAfter(endOfMarch)) {
+      const date = now.format("dddd, MMMM Do, YYYY");
+      const days = Math.ceil(moment.duration(now.diff(endOfMarch)).as("days"));
 
-    msg.send(`Today is ${dow}, ${isEvermarch ? 'Evermarch' : 'March'} ${days}, 2020`);
+      msg.send(
+        `Today is ${date}. It has been ${days} day${
+          days === 1 ? "" : "s"
+        } since March 2020.`
+      );
+    } else {
+      const isBlursday = Math.random() < 0.2;
+      const isEvermarch = Math.random() < 0.2;
+
+      const dow = isBlursday ? "Blursday" : now.format("dddd");
+      const days = Math.ceil(moment.duration(now.diff(march1)).as("days"));
+
+      msg.send(
+        `Today is ${isBlursday ? "Blursday" : dow}, ${
+          isEvermarch ? "Evermarch" : "March"
+        } ${days}, 2020`
+      );
+    }
   });
 };
