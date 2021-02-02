845efa94d82d685b212c52b05a87ff74c34ad52a
Greg Walker <michael.walker@gsa.gov>
2021-02-02 10:27:51 -0600

    all the code is done

--- src/scripts/erg-inviter.js
+++ src/scripts/erg-inviter.js
@@ -2,10 +2,56 @@ const fs = require("fs");
 const path = require("path");
 const yaml = require("js-yaml");
 
-const getERGs = () => {
+const getAllChannels = async (robot, cursor) =>
+  new Promise((resolve, reject) => {
+    const options = {
+      exclude_archived: true,
+      types: "public_channel,private_channel",
+    };
+    if (cursor) {
+      options.cursor = cursor;
+    }
+
+    robot.adapter.client.web.conversations.list(
+      options,
+      async (err, conversations) => {
+        if (err || !conversations.ok) {
+          return reject();
+        }
+
+        const list = conversations.channels.reduce(
+          (all, { id, name }) => ({
+            ...all,
+            [name]: id,
+          }),
+          {}
+        );
+
+        if (
+          conversations.response_metadata &&
+          conversations.response_metadata.next_cursor
+        ) {
+          const nextPage = await getAllChannels(
+            robot,
+            conversations.response_metadata.next_cursor
+          );
+          Object.assign(list, nextPage);
+        }
+
+        return resolve(list);
+      }
+    );
+  });
+
+const getERGs = async (robot) => {
   // Read in the huge list of bots from the Yaml file
   const ymlStr = fs.readFileSync(path.join(__dirname, "erg-inviter.yaml"));
   const { ergs } = yaml.safeLoad(ymlStr, { json: true });
+
+  const channels = await getAllChannels(robot);
+  Object.values(ergs).forEach((erg) => {
+    erg.channelId = channels[erg.channel]; // eslint-disable-line no-param-reassign
+  });
 
   return ergs;
 };
@@ -25,7 +71,7 @@ const getMessageText = async (robot, room, timestamp) =>
   });
 
 module.exports = async (robot) => {
-  const ergs = await getERGs();
+  const ergs = await getERGs(robot);
 
   const messages = Object.entries(ergs).map(
     ([name, { description }]) => `• *${name}*: ${description}`
@@ -54,14 +100,33 @@ module.exports = async (robot) => {
     }) => {
       if (type === "added" && !!isReactionToBot) {
         const text = await getMessageText(robot, room, ts);
-        const match = text.match(/• \*([^*]+)\*/);
-        if (match && ergs[match[1]]) {
-          const { channel } = ergs[match[1]];
+        const requestMatch = text.match(/^• \*([^*]+)\*/);
+
+        if (requestMatch && ergs[requestMatch[1]]) {
+          const { channelId } = ergs[requestMatch[1]];
           robot.messageRoom(
-            channel,
+            channelId,
             `<@${userID}> has requested to join this channel. Add an emoji reaction to this message to invite them.`
           );
-          console.log(userID);
+          return;
+        }
+
+        console.log(text);
+
+        const inviteMatch = text.match(
+          /^<@([^>]+)> has requested to join this channel\./
+        );
+        console.log(inviteMatch);
+
+        if (inviteMatch) {
+          const known = Object.values(ergs).some(
+            ({ channelId }) => channelId === room
+          );
+          if (known) {
+            const [, userId] = inviteMatch;
+
+            robot.adapter.client.web.conversations.invite(room, userId);
+          }
         }
       }
     }
