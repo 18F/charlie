dfd5121f265b191f752884f9b0e6926945f50599
Greg Walker <michael.walker@gsa.gov>
2021-03-01 15:09:57 -0600

    dad jokes

---
+++ src/scripts/dad-joke.js
@@ -0,0 +1,42 @@
+const { directMention } = require("@slack/bolt");
+const axios = require("axios");
+const { cache } = require("../utils");
+
+module.exports = (app) => {
+  app.message(directMention(), /dad joke/i, async ({ say }) => {
+    const jokes = await cache("dad jokes", 60, async () => {
+      try {
+        const { data } = await axios.get(
+          "https://fatherhood.gov/jsonapi/node/dad_jokes"
+        );
+
+        if (data && data.data) {
+          return data.data.map((joke) => ({
+            setup: joke.attributes.field_joke_opener,
+            punchline: joke.attributes.field_joke_response,
+          }));
+        }
+        return [];
+      } catch (e) {
+        return [];
+      }
+    });
+
+    const joke = jokes[Math.floor(Math.random() * jokes.length)];
+    if (joke) {
+      say({
+        icon_emoji: ":dog-joke:",
+        text: joke.setup,
+        username: "Charlie the Dad",
+      });
+
+      setTimeout(() => {
+        say({
+          icon_emoji: ":dog-joke:",
+          text: joke.punchline,
+          username: "Charlie the Dad",
+        });
+      }, 5000);
+    }
+  });
+};
---
+++ src/scripts/dad-joke.test.js
@@ -0,0 +1,146 @@
+const sinon = require("sinon");
+const {
+  axios,
+  getApp,
+  utils: { cache },
+} = require("../utils/test");
+
+const script = require("./dad-joke");
+
+describe("dad jokes (are the best worst)", () => {
+  const app = getApp();
+  let clock;
+
+  beforeAll(() => {
+    clock = sinon.useFakeTimers();
+  });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    clock.restore();
+  });
+
+  it("subscribes to dad joke requests", () => {
+    script(app);
+    expect(app.message).toHaveBeenCalledWith(
+      expect.any(Function),
+      /dad joke/i,
+      expect.any(Function)
+    );
+  });
+
+  describe("response to joke requests", () => {
+    let handler;
+    beforeEach(() => {
+      script(app);
+      handler = app.getHandler();
+    });
+
+    const message = { say: jest.fn() };
+
+    it("fetches jokes from a cache", async () => {
+      cache.mockResolvedValue([]);
+      await handler(message);
+      expect(cache).toHaveBeenCalledWith("dad jokes", 60, expect.any(Function));
+    });
+
+    describe("gets jokes from fatherhood.gov if the cache is expired or whatever", () => {
+      let fetch;
+      beforeEach(async () => {
+        cache.mockResolvedValue([]);
+        await handler(message);
+        fetch = cache.mock.calls[0][2];
+      });
+
+      it("if the API throws an error", async () => {
+        axios.get.mockRejectedValue("error");
+        const out = await fetch();
+
+        expect(out).toEqual([]);
+      });
+
+      it("if the API returns a malformed object", async () => {
+        axios.get.mockResolvedValue({ data: { data_is_missing: [] } });
+        const out = await fetch();
+
+        expect(out).toEqual([]);
+      });
+
+      it("if the API doesn't return any jokes", async () => {
+        axios.get.mockResolvedValue({ data: { data: [] } });
+        const out = await fetch();
+
+        expect(out).toEqual([]);
+      });
+
+      it("if the API does return some jokes", async () => {
+        axios.get.mockResolvedValue({
+          data: {
+            data: [
+              {
+                attributes: {
+                  field_joke_opener: "setup 1",
+                  field_joke_response: "punchline 1",
+                },
+              },
+              {
+                attributes: {
+                  field_joke_opener: "setup 2",
+                  field_joke_response: "punchline 2",
+                },
+              },
+              {
+                attributes: {
+                  field_joke_opener: "setup 3",
+                  field_joke_response: "punchline 3",
+                },
+              },
+            ],
+          },
+        });
+        const out = await fetch();
+
+        expect(out).toEqual([
+          { setup: "setup 1", punchline: "punchline 1" },
+          { setup: "setup 2", punchline: "punchline 2" },
+          { setup: "setup 3", punchline: "punchline 3" },
+        ]);
+      });
+    });
+
+    describe("responds with a joke", () => {
+      it("unless there aren't any jokes", async () => {
+        cache.mockResolvedValue([]);
+        await handler(message);
+        expect(message.say).not.toHaveBeenCalled();
+      });
+
+      it("if there is at least one joke", async () => {
+        cache.mockResolvedValue([
+          { setup: "joke setup here", punchline: "the funny part" },
+        ]);
+        await handler(message);
+
+        expect(message.say).toHaveBeenCalledWith({
+          icon_emoji: ":dog-joke:",
+          text: "joke setup here",
+          username: "Charlie the Dad",
+        });
+
+        // Ensure the punchline part comes after a delay by clearing out the
+        // existing mock calls.
+        message.say.mockClear();
+        clock.tick(5000);
+
+        expect(message.say).toHaveBeenCalledWith({
+          icon_emoji: ":dog-joke:",
+          text: "the funny part",
+          username: "Charlie the Dad",
+        });
+      });
+    });
+  });
+});
