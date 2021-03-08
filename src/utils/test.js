8f1d97fe37963fe746e86b280aa777ab1ca5c042
Greg Walker <mgwalker@users.noreply.github.com>
2021-03-08 10:04:34 -0600

    dedupe and clean up test util a bit (#281)

--- src/utils/test.js
+++ src/utils/test.js
@@ -20,16 +20,27 @@ module.exports = {
       message,
 
       /**
+       * Get the action handler that the bot registers.
+       * @param {Number} index Optional. The index of the handler to fetch. For
+       *    Bots that register multiple handlers. This is in the order that the
+       *    handlers are registered. Defaults to 0, the first handler.
+       */
+      getActionHandler: (index = 0) => {
+        if (action.mock.calls.length > index) {
+          return action.mock.calls[index].slice(-1).pop();
+        }
+        return null;
+      },
+
+      /**
        * Get the handler that the bot registers.
        * @param {Number} index Optional. The index of the handler to fetch. For
        *    Bots that register multiple handlers. This is in the order that the
        *    handlers are registered. Defaults to 0, the first handler.
        */
       getHandler: (index = 0) => {
-        if (message.mock.calls.length > 0) {
-          return message.mock.calls[index][
-            message.mock.calls[index].length - 1
-          ];
+        if (message.mock.calls.length > index) {
+          return message.mock.calls[index].slice(-1).pop();
         }
         return null;
       },
