aa375d7ece971c081de652857ef0f406a283cb58
Greg Walker <michael.walker@gsa.gov>
2021-06-11 14:01:44 -0500

    lint fixes for updated rules

--- src/utils/optOut.js
+++ src/utils/optOut.js
@@ -2,29 +2,27 @@ const brain = require("../brain");
 
 const BRAIN_KEY = "OPT_OUT";
 
-const optOut = (name) => {
-  return {
-    button: {
-      accessory: {
-        action_id: "opt_out",
-        type: "overflow",
-        options: [
-          {
-            text: {
-              type: "plain_text",
-              text: "Don't show me this anymore",
-            },
-            value: name,
+const optOut = (name) => ({
+  button: {
+    accessory: {
+      action_id: "opt_out",
+      type: "overflow",
+      options: [
+        {
+          text: {
+            type: "plain_text",
+            text: "Don't show me this anymore",
           },
-        ],
-      },
+          value: name,
+        },
+      ],
     },
-    isOptedOut: (userId) => {
-      const optOutList = brain.get(BRAIN_KEY) || {};
-      return (optOutList[name] || []).includes(userId);
-    },
-  };
-};
+  },
+  isOptedOut: (userId) => {
+    const optOutList = brain.get(BRAIN_KEY) || {};
+    return (optOutList[name] || []).includes(userId);
+  },
+});
 
 module.exports = optOut;
 module.exports.BRAIN_KEY = BRAIN_KEY;
