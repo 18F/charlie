657ae365dea017df6721f972bb8486bcb3dbd240
Greg Walker <michael.walker@gsa.gov>
2021-09-03 13:52:33 -0500

    update tests

--- src/scripts/inclusion-bot.test.js
+++ src/scripts/inclusion-bot.test.js
@@ -104,14 +104,21 @@ triggers:
     const expectedMessage = {
       attachments: [
         {
-          color: "#ffbe2e",
-          fallback: "fallback",
+          color: "#2eb886",
+          fallback: "This is the message",
           blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "This is the message",
+              },
+            },
             {
               accessory: {
                 action_id: "inclusion_modal",
                 text: {
-                  text: "What's this?",
+                  text: "Why this suggestion?",
                   type: "plain_text",
                 },
                 type: "button",
@@ -123,12 +130,16 @@ triggers:
               },
               type: "section",
             },
+            {
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: "You can view the <https://docs.google.com/document/d/1iQT7Gy0iQa7sopBP0vB3CZ56GhyYrDNUzLdoWOowSHs/edit|full list of words and phrases> this bot watches for.",
+                },
+              ],
+            },
           ],
-        },
-        {
-          color: "#2eb886",
-          text: "This is the message",
-          fallback: "This is the message",
         },
       ],
       icon_emoji: ":tts:",
@@ -150,9 +161,9 @@ triggers:
 
       expect(addEmojiReaction).toHaveBeenCalledWith(msg, "inclusion-bot");
 
-      expectedMessage.attachments[0].blocks[0].text.text =
+      expectedMessage.attachments[0].blocks[1].text.text =
         expect.stringMatching(
-          /• Instead of saying "match 1," how about \*(a1|a2|a3)\*?/
+          /• Instead of saying "match 1," how about \*(a1|a2|a3)\*\?/
         );
       expect(postEphemeralResponse).toHaveBeenCalledWith(msg, expectedMessage);
     });
@@ -169,47 +180,38 @@ triggers:
       msg.message.text = "hello this is the match 1 trigger and match 2a";
       handler(msg);
 
-      expectedMessage.attachments[0].blocks[0].accessory.value =
+      expectedMessage.attachments[0].blocks[1].accessory.value =
         "match 1|match 2a";
-      expectedMessage.attachments[0].blocks.push({
-        type: "section",
-        text: { type: "mrkdwn", text: "" },
-      });
 
       expect(addEmojiReaction).toHaveBeenCalledWith(msg, "inclusion-bot");
 
-      expectedMessage.attachments[0].blocks[0].text.text =
-        expect.stringMatching(
-          /• Instead of saying "match 1," how about \*(a1|a2|a3)\*?/
-        );
       expectedMessage.attachments[0].blocks[1].text.text =
         expect.stringMatching(
-          /• Instead of saying "match 2a," how about \*b1\*?/
+          /• Instead of saying "match 1," how about \*(a1|a2|a3)\*\?\n• Instead of saying "match 2a," how about \*b1\*\?/
         );
 
       expect(postEphemeralResponse).toHaveBeenCalledWith(msg, expectedMessage);
 
       // Reset the parts of the expected message that we changed above.
-      expectedMessage.attachments[0].blocks[0].accessory.value = "match 1";
-      expectedMessage.attachments[0].blocks.splice(1, 1);
+      expectedMessage.attachments[0].blocks[1].accessory.value = "match 1";
     });
 
     it("handles two triggering phrases where one is explicitly ignored", () => {
       msg.message.text = "hello this is the not match 1 trigger and match 2a";
       handler(msg);
 
-      expectedMessage.attachments[0].blocks[0].accessory.value = "match 2a";
+      expectedMessage.attachments[0].blocks[1].accessory.value = "match 2a";
 
       expect(addEmojiReaction).toHaveBeenCalledWith(msg, "inclusion-bot");
 
-      expectedMessage.attachments[0].blocks[0].text.text =
+      expectedMessage.attachments[0].blocks[1].text.text =
         expect.stringMatching(
-          /• Instead of saying "match 2a," how about \*b1\*?/
+          /• Instead of saying "match 2a," how about \*b1\*\?/
         );
       expect(postEphemeralResponse).toHaveBeenCalledWith(msg, expectedMessage);
 
       // Reset the parts of the expected message that we changed above.
-      expectedMessage.attachments[0].blocks[0].accessory.value = "match 1";
+      expectedMessage.attachments[0].blocks[1].accessory.value = "match 1";
     });
 
     it("does not trigger on phrases that are contained within quotes", () => {
@@ -224,18 +226,18 @@ triggers:
       msg.message.text = `here is "match 1" but match 2a is not quoted`;
       handler(msg);
 
-      expectedMessage.attachments[0].blocks[0].accessory.value = "match 2a";
+      expectedMessage.attachments[0].blocks[1].accessory.value = "match 2a";
 
       expect(addEmojiReaction).toHaveBeenCalledWith(msg, "inclusion-bot");
 
-      expectedMessage.attachments[0].blocks[0].text.text =
+      expectedMessage.attachments[0].blocks[1].text.text =
         expect.stringMatching(
-          /• Instead of saying "match 2a," how about \*b1\*?/
+          /• Instead of saying "match 2a," how about \*b1\*\?/
         );
       expect(postEphemeralResponse).toHaveBeenCalledWith(msg, expectedMessage);
 
       // Reset the parts of the expected message that we changed above.
-      expectedMessage.attachments[0].blocks[0].accessory.value = "match 1";
+      expectedMessage.attachments[0].blocks[1].accessory.value = "match 1";
     });
   });
 });
