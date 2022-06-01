e4a31df2e0e2fe5f32420b8e4f8c86fa44dbee4b
James Moffet <jimmoffet@gmail.com>
2022-06-01 12:54:48 -0700

    use qs instead of form-urlencoded

--- src/spoof-message.js
+++ src/spoof-message.js
@@ -1,7 +1,8 @@
-require("./env");
-const crypto = require('crypto');
+const crypto = require("crypto");
 const axios = require("axios");
-// const formurlencoded = require('form-urlencoded');
+require("./env");
+const qs = require('qs');
+
 
 // NOTES: bolt's App object requires valid credentials to initialize, so .env must contain valid credentials
 
@@ -17,13 +18,13 @@ function calculateSignature(
   let rawBody;
   if (contentType?.toLocaleLowerCase() === 'application/x-www-form-urlencoded') {
     // Slash commands are sent in this content type
-    rawBody = formurlencoded(body);
+    rawBody = qs.stringify(rawBody,{ format:'RFC1738' })
   } else {
-    var stringBody = JSON.stringify(body);
-    rawBody = stringBody.replace(/\//g, '\\/').replace(/[\u007f-\uffff]/g, (c) => '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4));
+    const stringBody = JSON.stringify(body);
+    rawBody = stringBody.replace(/\//g, '\\/').replace(/[\u007f-\uffff]/g, (c) => `\\u${  (`0000${  c.charCodeAt(0).toString(16)}`).slice(-4)}`);
   }
   const basestring = ['v0', timestampHeader, rawBody].join(':');
-  const calculatedSignature = 'v0=' + crypto.createHmac('sha256', signingSecret).update(basestring).digest('hex');
+  const calculatedSignature = `v0=${  crypto.createHmac('sha256', signingSecret).update(basestring).digest('hex')}`;
   if (signatureToCompare) {
     // still broken
     const calculatedSignatureBuffer = Buffer.from(calculatedSignature, 'utf8');
@@ -37,12 +38,12 @@ function calculateSignature(
 function spoofMessage(
   msg,
   token = "52YK4N4phElR07An4dC59yMR", // optional
-  team_id = "TUMHGJ48G", // optional
+  teamID = "TUMHGJ48G", // optional
   channel = "CUM5CUN5A", // optional
-  api_app_id = "A02N5BNE13L", // optional
-  user_id = "UUPDE22F9", // optional
-  event_id = "Ev03HC9TGA86", // optional
-  auth_user_id = "U02N2D5FRB7", // optional
+  apiAppID = "A02N5BNE13L", // optional
+  userID = "UUPDE22F9", // optional
+  eventID = "Ev03HC9TGA86", // optional
+  authUserID = "U02N2D5FRB7", // optional
   ) {
 
   const now = Date.now();
@@ -50,13 +51,13 @@ function spoofMessage(
   const timestampHeader = nowSeconds.toString() // Get it from the request header x-slack-request-timestamp
   const inputBody = {
     "token": token,
-    "team_id": team_id,
-    "api_app_id": api_app_id,
+    "team_id": teamID,
+    "api_app_id": apiAppID,
     "event": {
       "client_msg_id": "220923fe-ac54-474b-839c-4aed523035eb",
       "type": "message",
       "text": msg,
-      "user": user_id,
+      "user": userID,
       "ts": "1653938696.488369",
       "team": "TUMHGJ48G",
       "blocks": [{
@@ -75,12 +76,12 @@ function spoofMessage(
       "channel_type": "channel"
     },
     "type": "event_callback",
-    "event_id": event_id,
+    "event_id": eventID,
     "event_time": nowSeconds,
     "authorizations": [{
       "enterprise_id": null,
-      "team_id": team_id,
-      "user_id": auth_user_id,
+      "team_id": teamID,
+      "user_id": authUserID,
       "is_bot": true,
       "is_enterprise_install": false
     }],
@@ -90,7 +91,7 @@ function spoofMessage(
   const contentType = "application/json" // Get it from the request header content-type
   const signingSecret = process.env.SLACK_SIGNING_SECRET
   
-  let sig = calculateSignature(
+  const sig = calculateSignature(
     timestampHeader,
     contentType,
     signingSecret,
@@ -99,7 +100,7 @@ function spoofMessage(
 
   const data = JSON.stringify(inputBody);
 
-  const res = axios.post('http://localhost:3000/slack/events', inputBody, {
+  axios.post('http://localhost:3000/slack/events', inputBody, {
     headers: {
       'Content-Type': 'application/json',
       'Content-Length': data.length,
