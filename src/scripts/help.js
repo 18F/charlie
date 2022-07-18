2b6cbb8b942d2a9ce46dcc6eb12415c0a8b81162
Greg Walker <michael.walker@gsa.gov>
2022-07-18 14:08:31 -0500

    bug with abusing the slack api

--- src/scripts/help.js
+++ src/scripts/help.js
@@ -79,11 +79,42 @@ module.exports = async (app) => {
       // get rid of the last divider
       blocks.pop();
 
-      say({
-        blocks,
-        text: "Charlie help",
-        thread_ts: thread ?? ts,
-      });
+      const messages = [];
+
+      // The Slack API only allows up to 50 blocks per message. If we have more
+      // than 50 blocks, then, we need to break them up into multiple messages.
+      do {
+        const subset = blocks.splice(0, 50);
+
+        // After removing the subset, if the first remaining block is a divider,
+        // throw it out. We don't need to start messages with a divider.
+        if (blocks.length > 0 && blocks[0].type === "divider") {
+          blocks.shift();
+        }
+
+        // If the first or last block of the subset is a divider, toss it.
+        if (subset[0].type === "divider") {
+          subset.shift();
+        }
+        if (subset[subset.length - 1].type === "divider") {
+          subset.pop();
+        }
+
+        // Add this subset to the list of messages to send.
+        messages.push({
+          blocks: subset,
+          text: "Charlie help",
+          thread_ts: thread ?? ts,
+        });
+
+        // Because the subset is spliced from the original blocks, we can just
+        // keep iterating until there aren't any blocks left.
+      } while (blocks.length > 0);
+
+      // Send all the messages
+      for await (const message of messages) {
+        await say(message);
+      }
     }
   );
 };
