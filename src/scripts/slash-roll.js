8a66c646e9572eb8fa2c94dd2450af13b2432351
Greg Walker <greg.walker@gsa.gov>
2023-02-07 17:27:33 -0600

    Tadhg did stuff, who knows

--- src/scripts/slash-roll.js
+++ src/scripts/slash-roll.js
@@ -3,13 +3,19 @@ module.exports = async (app) => {
     await ack();
 
     const dieRoll = (number) => Math.floor(Math.random() * number) + 1;
+    /* parse command.text
+    Simplest case: d[whatever]
+    */
+
+    const [, sides] = command.text.match(/d([0-9]{1,4})/) ?? [];
+    const actualSides = sides ? Number.parseInt(sides, 10) : 20;
 
     await say({
       // icon_emoji: "",
       // username: "",
       text: `<@${command.user_id}> rolled ${
         command.text
-      }, resulting in ${dieRoll(20)}`,
+      }, resulting in ${dieRoll(actualSides)}`,
     });
   });
 };
