00b2808d95eb809869123a7121825dbf07f49652
Greg Walker <greg.walker@gsa.gov>
2023-02-07 16:25:51 -0600

    placeholder slash command

---
+++ src/scripts/slash-roll.js
@@ -0,0 +1,17 @@
+module.exports = async (app) => {
+  app.command(
+    "/roll",
+    async ({ command, ack, message: { thread_ts: threadId }, say }) => {
+      await ack();
+
+      // command.text; // the user typed this maybe?
+
+      await say({
+        // icon_emoji: "",
+        // username: "",
+        text: "You rolled a 3",
+        thread_ts: threadId,
+      });
+    }
+  );
+};
