8012e45c7b14a34f37b3fbf81797f45e032d62bb
Greg Walker <greg.walker@gsa.gov>
2023-02-07 17:05:58 -0600

    console.log because we're cavepeople

--- src/scripts/slash-roll.js
+++ src/scripts/slash-roll.js
@@ -1,19 +1,17 @@
 module.exports = async (app) => {
-  app.command(
-    "/roll",
-    async ({ command, ack, /* message: { thread_ts: threadId }, */ say }) => {
-      await ack();
+  app.command("/roll", async ({ ack, say, ...rest }) => {
+    await ack();
 
-      const dieRoll = (number) => Math.floor(Math.random() * number) + 1;
+    const dieRoll = (number) => Math.floor(Math.random() * number) + 1;
 
-      // command.text; // the user typed this maybe?
+    // command.text; // the user typed this maybe?
+    console.log(rest);
 
-      await say({
-        // icon_emoji: "",
-        // username: "",
-        text: `You rolled a ${dieRoll(20)}`,
-        // thread_ts: threadId,
-      });
-    }
-  );
+    await say({
+      // icon_emoji: "",
+      // username: "",
+      text: `You rolled a ${dieRoll(20)}`,
+      // thread_ts: threadId,
+    });
+  });
 };
