4d75ecb0d79c02f78ca23ba173ef7f836f8357d2
Greg Walker <greg.walker@gsa.gov>
2023-02-07 16:53:46 -0600

    add randomness

--- src/scripts/slash-roll.js
+++ src/scripts/slash-roll.js
@@ -4,12 +4,14 @@ module.exports = async (app) => {
     async ({ command, ack, /* message: { thread_ts: threadId }, */ say }) => {
       await ack();
 
+      const dieRoll = (number) => Math.floor(Math.random() * number) + 1;
+
       // command.text; // the user typed this maybe?
 
       await say({
         // icon_emoji: "",
         // username: "",
-        text: "You rolled a 3",
+        text: `You rolled a dieRoll(20)`,
         // thread_ts: threadId,
       });
     }
