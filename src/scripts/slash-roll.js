e23dbe0d33ce81bb5ac3df6a99eb21dcda2be650
Greg Walker <greg.walker@gsa.gov>
2023-02-07 17:14:53 -0600

    real roll!

--- src/scripts/slash-roll.js
+++ src/scripts/slash-roll.js
@@ -1,17 +1,15 @@
 module.exports = async (app) => {
-  app.command("/roll", async ({ ack, say, ...rest }) => {
+  app.command("/roll", async ({ ack, say, command }) => {
     await ack();
 
     const dieRoll = (number) => Math.floor(Math.random() * number) + 1;
 
-    // command.text; // the user typed this maybe?
-    console.log(rest);
-
     await say({
       // icon_emoji: "",
       // username: "",
-      text: `You rolled a ${dieRoll(20)}`,
-      // thread_ts: threadId,
+      text: `<@${command.user_id}> rolled ${
+        command.text
+      }, resulting in ${dieRoll(20)}`,
     });
   });
 };
