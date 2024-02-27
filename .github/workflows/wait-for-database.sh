1e5821bb8c547cb26cb041d6b6f116f60d11cb6e
Greg Walker <greg.walker@gsa.gov>
2024-02-27 09:16:26 -0600

    don't need this file

--- .github/workflows/wait-for-database.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-STATUS="$(cf service charlie-brain | grep "  status:" | awk -F ":" '{print $2}' | xargs)"
-while [ "$STATUS"  != "create succeeded" ]
-do
-  echo "Waiting for database service to be ready..."
-  sleep 10
-  $STATUS=$(cf service charlie-brain | grep "  status:" | awk -F ":" '{print $2}' | xargs)
-done
