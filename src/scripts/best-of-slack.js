573810ca356b0d3daa6d5a1dfac6b4d0ebe5c2a0
Greg Walker <greg.walker@gsa.gov>
2024-12-11 15:15:01 -0600

    check emoji

--- src/scripts/best-of-slack.js
+++ src/scripts/best-of-slack.js
@@ -1,17 +1,37 @@
+const bestOfSlack = new Set();
+
+const getPermalink = async (event, client) =>
+  client.chat
+    .getPermalink({
+      channel: event.item.channel,
+      message_ts: event.item.ts,
+    })
+    .then((result) => result.permalink);
+
+// const EMOJI = ":best-of-slack-award:";
+const EMOJI = ":koala:";
+
+// Events:
+//   * reaction_added
+//   * reaction_removed
+//
+// Scopes:
+//   * reactions:read
+
 module.exports = async (app) => {
-  app.event(
-    "reaction_added",
-    async ({
-      event: {
-        item: { channel, ts },
-      },
-      client,
-    }) => {
-      const link = await client.chat.getPermalink({
-        channel,
-        message_ts: ts,
-      });
-      console.log(link);
-    },
-  );
+  app.event("reaction_added", async ({ event, client }) => {
+    const { reaction } = event;
+    if (`:${reaction}:` === EMOJI) {
+      const permalink = await getPermalink(event, client);
+      bestOfSlack.add(permalink);
+    }
+  });
+
+  app.event("reaction_removed", async ({ event, client }) => {
+    const { reaction } = event;
+    if (`:${reaction}:` === EMOJI) {
+      const permalink = await getPermalink(event, client);
+      bestOfSlack.delete(permalink);
+    }
+  });
 };
